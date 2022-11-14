90e16b188136a84c2ff292f9a89266286acd502c
unknown <aidenperse@gmail.com>
2022-11-14 11:22:43 -0500

    Agrego una nueva universidad

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Universidades EPN = new Universidades();
+        Universidades EPN = new Universidades(); //Objeto universidad 1
         System.out.println("Nombre: " + EPN.getNombre_U());
         System.out.println("Carreras: " + EPN.getCarreras());
         System.out.println("Docentes: " + EPN.getDocentes());
         System.out.println("Estudiantes: " + EPN.getEstudiantes());
+        Universidades Sale = new Universidades();
+        System.out.println(" ");
+        Sale.setNombre_U("Salesiana");
+        System.out.println("Nombre: " + Sale.getNombre_U());
+        Sale.setCarreras(5);
+        System.out.println("Carreras: " + Sale.getCarreras());
+        Sale.setDocentes(8);
+        System.out.println("Docentes: " + Sale.getDocentes());
+        Sale.setEstudiantes(50);
+        System.out.println("Estudiantes: " + Sale.getEstudiantes());
     }
 }
