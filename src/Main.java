1491b3971d4fa2e55743629e96b882e09716f0f4
Trabajo_grupal <dilan.flores@epn.edu.ec>
2022-11-14 11:21:24 -0500

    Cambios en datos

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         System.out.println("Carreras: " + EPN.getCarreras());
         System.out.println("Docentes: " + EPN.getDocentes());
         System.out.println("Estudiantes: " + EPN.getEstudiantes());
+
+        System.out.println("    Cambios!");
+        System.out.println("Nombre: " + EPN.getNombre_U());
+        EPN.setCarreras(10);
+        System.out.println("Carreras: " + EPN.getCarreras());
+        EPN.setDocentes(5);
+        System.out.println("Docentes: " + EPN.getDocentes());
+        EPN.setEstudiantes(200);
+        System.out.println("Estudiantes: " + EPN.getEstudiantes());
+
+
     }
 }
